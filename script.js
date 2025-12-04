2c18e42c35e48977c585e56a125b5c76729c4beb
lieik1209-beep <lieik1209@gmail.com>
2025-12-04 15:31:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.getElementById('btnO').addEventListener('click', () => {
+  alert('당신 뭘좀 아시는군요? 😊');
+});
+
+document.getElementById('btnX').addEventListener('click', () => {
+  alert('너가 뭘 알아 썌꺄😤');
+});
