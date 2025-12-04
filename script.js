9df4cf0196550ace7120bb22b6370404a93b4ec3
lieik1209-beep <lieik1209@gmail.com>
2025-12-04 16:18:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const audioO = document.getElementById('audioO');
 
 document.getElementById('btnO').addEventListener('click', () => {
   alert('당신 뭘좀 아시는군요? 😊');
+  
+  const audioO = document.getElementById('audioO');
   audioO.play();
+
+  const img = document.getElementById('showImg');
+  img.src = "111.jpg";
+  img.style.display = "block";
 });
 
+
 document.getElementById('btnX').addEventListener('click', () => {
-  alert('거짓말은 나빠요 😤');
+  alert('너가 뭘알아 쌔꺄 😤');
 });
